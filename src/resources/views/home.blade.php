6d4786fb01583c6ab73bbab23dacd133272c429e
caxtonMuthoni <githinjicaxton323@gmail.com>
2020-11-16 04:03:04 +0300

    first commit

--- src/resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container-fluid">
-    <main-app :user="{{Auth::user() != null ? Auth::user() : 'no'}}" />
-</div>
-@endsection
--- src/resources/views/vote.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container-fluid">
-    <vote :user="{{Auth::user() != null ? Auth::user() : 'no'}}" />
-</div>
-@endsection
